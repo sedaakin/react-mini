fccec3a8ad81915318cca0a5b0d9fc971e71f785
sedaakin <81624327+sedaakin@users.noreply.github.com>
2022-01-31 21:26:54 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./component/List";
+import AddToList from "./component/AddToList"
 
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Lebron James",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 36,
+      note: "notes..."
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Liste</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
---
+++ src/component/List.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {IState as IProps} from "../App"
+
+const List: React.FC<IProps> = ({people}) => {
+    const renderList = ()=> {
+        return people.map((person)=>{
+            return (
+                <li className="List">
+                <div className="List-header">
+                    <img className="List-img" src={person.url}></img>
+                    <h2>{person.name}</h2>
+                </div>
+                <p>{person.age} years old</p>
+                <p className="List-note">{person.note}</p>
+            </li>
+            )
+        })
+    }
+    return (
+        <ul>
+            {renderList()}
+        </ul>
+    )
+}
+
+export default List;
